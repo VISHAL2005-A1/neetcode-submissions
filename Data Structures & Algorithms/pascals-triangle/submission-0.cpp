c60053ac887b0a4c9014d7623ecaea0ec27ca7b9
Vishal tech 321 <vishalgautam1118465@gmail.com>
2026-06-09 20:54:29 +0530

    Add: pascals-triangle - submission-0

---
+++ Data Structures & Algorithms/pascals-triangle/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        int n=numRows;
+        vector<vector<int>>ans(n);
+        for(int i=0;i<n;i++){
+            ans[i]=vector<int>(i+1,1);
+            for(int j=1;j<i;j++){
+                ans[i][j]=ans[i-1][j-1]+ans[i-1][j];
+            }
+        }
+        return ans;
+    }
+}; 
